d01739d1f1150bf93df36f4421b2b24d83635309
songrit maleerat <song.cpe@gmail.com>
2021-04-10 19:44:43 +0700

    Add test

---
+++ src/test/kotlin/SolutionTest.kt
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+internal class SolutionTest {
+    private val solution = Solution()
+
+    companion object {
+        @JvmStatic
+        fun testArguments(): Stream<Arguments> = Stream.of(
+            Arguments.of("abcabcbb!", 3),
+            Arguments.of("bbbbb", 1),
+            Arguments.of("pwwkew", 3),
+            Arguments.of("", 0),
+
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("testArguments")
+    fun `solution test`(case: String, expected: Int) {
+        Assertions.assertEquals(solution.lengthOfLongestSubstring(case), expected)
+    }
+}
